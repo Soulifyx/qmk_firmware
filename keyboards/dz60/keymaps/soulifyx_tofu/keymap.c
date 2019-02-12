cc2fcab10233330b4fd91f76cb284eef33c20858
Soulifyx <frozen_soul80@yahoo.com>
2019-02-12 22:12:21 +0700

    add reset button on backslash _CALC, add ctrl alt lrshift and remap RGB_LYR to tab location on _FKEYS, remove ^ to fix bug if RGB_LYR is pressed multiple times

--- keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
+++ keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
@@ -16,16 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_RSPC, KC_UP, KC_SLSH, 
     KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, TG(1), TG(2), KC_LEFT, KC_DOWN, KC_RGHT),
   [_FKEYS] = LAYOUT_60_b_ansi(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_DEL, 
-    KC_NO, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, KC_NO, KC_PSCR, 
+    RGB_LYR, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, KC_NO, KC_PSCR, 
     KC_NO, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, KC_NO, KC_NO, KC_NO, KC_ENT, 
-    KC_NO, BL_TOGG, BL_STEP, BL_ON, BL_OFF, BL_INC, BL_DEC, BL_BRTG, KC_NO, KC_NO, KC_NO, KC_VOLU, KC_MUTE, 
-    KC_NO, KC_LGUI, RGB_LYR, KC_SPC, KC_SPC, KC_SPC, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
+    KC_LSHIFT, BL_TOGG, BL_STEP, BL_ON, BL_OFF, BL_INC, BL_DEC, BL_BRTG, KC_NO, KC_NO, KC_RSHIFT, KC_VOLU, KC_MUTE, 
+    KC_LCTRL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
 	[_CALC] = LAYOUT_60_b_ansi(KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC, KC_BSPC, 
     KC_NO, KC_P7, KC_P8, KC_P9, KC_PCMM, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
     KC_NO, KC_P4, KC_P5, KC_P6, KC_PDOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PENT, 
-    KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_NO,
+    KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, RESET,
     KC_NO, KC_NO, KC_NO, KC_P0, KC_P0, KC_P0, KC_NO, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT)
-	
 };
 
 typedef union {
@@ -107,7 +106,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) { // Adds anothe
   switch (keycode) {
     case RGB_LYR:  // This allows me to use underglow as layer indication, or as normal
         if (record->event.pressed) { 
-            user_config.rgb_layer_change ^= 1; // Toggles the status
+            user_config.rgb_layer_change = 1; // Toggles the status
             eeconfig_update_user(user_config.raw); // Writes the new status to EEPROM
             if (user_config.rgb_layer_change) { // if layer state indication is enabled, 
                 layer_state_set(layer_state);   // then immediately update the layer color
