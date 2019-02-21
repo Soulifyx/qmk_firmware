211207797bab1a53ccd90e65f1a8eeb2500a24ad
Soulifyx <frozen_soul80@yahoo.com>
2019-02-21 18:17:12 +0700

    change email location, operators location

--- keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
+++ keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //        * |-----------------------------------------------------------|
  //  RGB   * |RGB_LY|Tog|MOD|mod|HUE|hue|SAT|sat|VAL|val|SPD|spd|  |Print|
  //        * |-----------------------------------------------------------|
- //  mode  * |#######| Pl| Br| Ra| Sw| Sn| Kn| Xm| Gr|   |EM1|EM2|###### |
+ //  mode  * |#######| Pl| Br| Ra| Sw| Sn| Kn| Xm| Gr|   |   |   |###### |
  //        * |-----------------------------------------------------------|
  //  led   * |#####   |Tog|Cyc| On|Off| +|  -| Br|   |   |#####|VOL|Mute |
  //        * |-----------------------------------------------------------|
@@ -113,30 +113,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FKEYS] = LAYOUT_60_b_ansi(
     KC_GRV,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,      KC_F12,      KC_DEL,      KC_DEL,      
     RGB_LYR,     RGB_TOG,    RGB_MOD,    RGB_RMOD,   RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_VAI,    RGB_VAD,    RGB_SPI,     RGB_SPD,     KC_NO,       KC_PSCR,      
-    KC_TRNS,     RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,   RGB_M_SN,   RGB_M_K,    RGB_M_X,    RGB_M_G,    KC_NO,      _EM1,         _EM2,       KC_TRNS,      
+    KC_TRNS,     RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,   RGB_M_SN,   RGB_M_K,    RGB_M_X,    RGB_M_G,    KC_NO,      KC_NO,       KC_NO,       KC_TRNS,      
     KC_TRNS,     BL_TOGG,    BL_STEP,    BL_ON,      BL_OFF,     BL_INC,     BL_DEC,     BL_BRTG,    KC_NO,      KC_NO,      KC_TRNS,     KC_VOLU,     KC_MUTE,      
     KC_TRNS,     KC_TRNS,    KC_LALT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MPRV,    KC_VOLD,    KC_MNXT),
 	
   /*
      Keymap: (_CALC Layer) Layer 2
    * ,--------------------------------------------------------------------,
-   * |### |## |## |## |## |## |## |## |## |## |## |## |## |     Backsp    |
+   * |### | / | * | - | + |## |## |## |## |## |## |## |## |     Backsp    |
    * |--------------------------------------------------------------------|
-   * |     |  7|  8|  9|   |   |   |   | / | * | - | + |   |    ######    |
+   * |     |  7|  8|  9| . |   |   |   |   |   |   |   |   |    ######    |
    * |--------------------------------------------------------------------|
-   * | NumLk |  4|  5|  6|   |   |   | . | , |   |   |   |      return    |
+   * | NumLk |  4|  5|  6| , |   |   |   |   |   |EM1|EM2|      return    |
    * |--------------------------------------------------------------------|
-   * |        |  1|  2|  3|   |   | = |   |   |   |   |       | ^ | RESET |
+   * |        |  1|  2|  3| = |   |   |   |   |   |   |       | ^ | RESET |
    * |--------------------------------------------------------------------|
    * |    |    |### |          0         |      |  ####  | <  | V |  >    |
    * `--------------------------------------------------------------------'
   */
 
   [_CALC] = LAYOUT_60_b_ansi(
-    KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSPC,      KC_BSPC,      
-    KC_NO,        KC_P7,        KC_P8,        KC_P9,        KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_PSLS,    KC_PAST,    KC_PMNS,    KC_PPLS,    KC_NO,      KC_TRNS,      
-    KC_NUMLOCK,   KC_P4,        KC_P5,        KC_P6,        KC_NO,        KC_NO,      KC_NO,      KC_PDOT,    KC_PCMM,    KC_NO,      KC_NO,      KC_NO,      KC_PENT,      
-    KC_NO,        KC_P1,        KC_P2,        KC_P3,        KC_NO,        KC_NO,      KC_PEQL,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,
+    KC_TRNS,      KC_PSLS,      KC_PAST,      KC_PMNS,      KC_PPLS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSPC,      KC_BSPC,      
+    KC_NO,        KC_P7,        KC_P8,        KC_P9,        KC_PDOT,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,      
+    KC_NUMLOCK,   KC_P4,        KC_P5,        KC_P6,        KC_PCMM,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      _EM1,       _EM2,       KC_PENT,      
+    KC_NO,        KC_P1,        KC_P2,        KC_P3,        KC_PEQL,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,
     KC_NO,        KC_NO,        KC_TRNS,      KC_P0,        KC_P0,        KC_P0,      KC_NO,      KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_RGHT)
 };
 
