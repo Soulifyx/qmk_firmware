a5685259d7cf40bc46bd817fd1131e84e1d7d3f6
Soulifyx <frozen_soul80@yahoo.com>
2019-03-24 20:46:33 +0700

    change toggle layer 1 to alt and swap /? with tg1

--- keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
+++ keyboards/dz60/keymaps/soulifyx_tofu/keymap.c
@@ -2,6 +2,10 @@
 #include "dz60.h"
 #include "led.h"
 
+#define BACKLIGHT_TIMEOUT 10    // in minutes
+#define _RSHIFT RSFT_T(KC_SLSH)
+#define _LALT TD(ALT_OSRAISE)
+
 enum keyboard_layers {
   _BASE = 0, // Base Layer 
   _FKEYS,      // Control Layer
@@ -44,7 +48,6 @@ user_config_t user_config;
 static int alttap_state = 0;
 
 // Backlight timeout feature
-#define BACKLIGHT_TIMEOUT 10    // in minutes
 static uint16_t idle_timer = 0;
 static uint8_t halfmin_counter = 0;
 bool backlight_status = true;
@@ -85,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------|
    * |CAPS/MO|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|      return    |
    * |--------------------------------------------------------------------|
-   * |Shift ( |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift )   | ^ | ?/ |
+   * |Shift ( |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  Shift ?/  | ^ | TG 1 |
    * |--------------------------------------------------------------------|
-   * |Ctrl|Gui |SPAlt|      Space           |  TG 1|  TG 2  | <  | V |  > |
+   * |Ctrl|Gui |SPAlt|      Space           |  Alt |  TG 2  | <  | V |  > |
    * `--------------------------------------------------------------------'
   */
 
@@ -95,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,      KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,      KC_EQL,      KC_BSPC,      KC_BSPC,      
     KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,      KC_RBRC,     KC_BSLS,      
     KC_CAPS,     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,      KC_ENT,      
-    KC_LSPO,     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_RSPC,   KC_UP,        KC_SLSH,      
-    KC_LCTL,     KC_LGUI,   TD(ALT_OSRAISE),   KC_SPC,    KC_SPC,    KC_SPC,    TG(_FKEYS),     TG(_CALC),     KC_LEFT,   KC_DOWN,   KC_RGHT),
+    KC_LSPO,     KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    _RSHIFT,   KC_UP,        TG(_FKEYS),      
+    KC_LCTL,     KC_LGUI,   _LALT,     KC_SPC,    KC_SPC,    KC_SPC,    KC_RALT,    TG(_CALC), KC_LEFT,   KC_DOWN,   KC_RGHT),
   
  // Keymap: (FKeys Layer) Layer 1
  //        * ,-----------------------------------------------------------.
@@ -106,17 +109,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //        * |-----------------------------------------------------------|
  //  mode  * |#######| Pl| Br| Ra| Sw| Sn| Kn| Xm| Gr|   |   |   |###### |
  //        * |-----------------------------------------------------------|
- //  led   * |#####   |Tog|Cyc| On|Off| +|  -| Br|   |   |#####|VOL|Mute |
+ //  led   * |#####   |Tog|Cyc| On|Off| +|  -| Br|   |   |#####|VOL|#### |
  //        * |-----------------------------------------------------------|
- //        * |####|### |Alt |      #####       |####|#### |Prev|vol |Next|
+ //        * |####|### |Alt |      #####       |Mute|#### |Prev|vol |Next|
  //        * `-----------------------------------------------------------'
 
   [_FKEYS] = LAYOUT_60_b_ansi(
     KC_GRV,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_F11,      KC_F12,      KC_DEL,      KC_DEL,      
-    RGB_LYR,     RGB_TOG,    RGB_MOD,    RGB_RMOD,   RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_VAI,    RGB_VAD,    RGB_SPI,     RGB_SPD,     KC_NO,       KC_PSCR,      
-    KC_TRNS,     RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,   RGB_M_SN,   RGB_M_K,    RGB_M_X,    RGB_M_G,    KC_NO,      KC_NO,       KC_NO,       KC_TRNS,      
-    KC_TRNS,     BL_TOGG,    BL_STEP,    BL_ON,      BL_OFF,     BL_INC,     BL_DEC,     BL_BRTG,    KC_NO,      KC_NO,      KC_TRNS,     KC_VOLU,     KC_MUTE,      
-    KC_TRNS,     KC_TRNS,    KC_LALT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MPRV,    KC_VOLD,    KC_MNXT),
+    RGB_LYR,     RGB_TOG,    RGB_MOD,    RGB_RMOD,   RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_VAI,    RGB_VAD,    RGB_SPI,     RGB_SPD,     XXXXXXX,     KC_PSCR,      
+    KC_TRNS,     RGB_M_P,    RGB_M_B,    RGB_M_R,    RGB_M_SW,   RGB_M_SN,   RGB_M_K,    RGB_M_X,    RGB_M_G,    XXXXXXX,    XXXXXXX,     XXXXXXX,     KC_TRNS,      
+    KC_TRNS,     BL_TOGG,    BL_STEP,    BL_ON,      BL_OFF,     BL_INC,     BL_DEC,     BL_BRTG,    XXXXXXX,    XXXXXXX,    KC_TRNS,     KC_VOLU,     KC_TRNS,      
+    KC_TRNS,     KC_TRNS,    KC_LALT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MUTE,    KC_TRNS,    KC_MPRV,    KC_VOLD,    KC_MNXT),
 	
   /*
      Keymap: (_CALC Layer) Layer 2
@@ -127,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------|
    * | NumLk |  4|  5|  6| , |   |   |   |   |   |EM1|EM2|      return    |
    * |--------------------------------------------------------------------|
-   * |        |  1|  2|  3| = |   |   |   |   |   |   |       | ^ | RESET |
+   * |        |  1|  2|  3| = |   |   |   |   |   |   |       | ^ |       |
    * |--------------------------------------------------------------------|
    * |    |    |### |          0         |      |  ####  | <  | V |  >    |
    * `--------------------------------------------------------------------'
@@ -135,10 +138,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CALC] = LAYOUT_60_b_ansi(
     KC_TRNS,      KC_PSLS,      KC_PAST,      KC_PMNS,      KC_PPLS,      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSPC,      KC_BSPC,      
-    KC_NO,        KC_P7,        KC_P8,        KC_P9,        KC_PDOT,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_TRNS,      
-    KC_NUMLOCK,   KC_P4,        KC_P5,        KC_P6,        KC_PCMM,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      _EM1,       _EM2,       KC_PENT,      
-    KC_NO,        KC_P1,        KC_P2,        KC_P3,        KC_PEQL,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,
-    KC_NO,        KC_NO,        KC_TRNS,      KC_P0,        KC_P0,        KC_P0,      KC_NO,      KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_RGHT)
+    XXXXXXX,      KC_P7,        KC_P8,        KC_P9,        KC_PDOT,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_TRNS,      
+    KC_NUMLOCK,   KC_P4,        KC_P5,        KC_P6,        KC_PCMM,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _EM1,       _EM2,       KC_PENT,      
+    XXXXXXX,      KC_P1,        KC_P2,        KC_P3,        KC_PEQL,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_UP,      XXXXXXX,
+    XXXXXXX,      XXXXXXX,      KC_TRNS,      KC_P0,        KC_P0,        KC_P0,      XXXXXXX,    KC_TRNS,    KC_LEFT,    KC_DOWN,    KC_RGHT)
 };
 
 void matrix_init_kb(void) {
@@ -200,6 +203,11 @@ void led_init_ports(void) {
 void led_set_kb(uint8_t usb_led) {
     layer = biton32(layer_state);
     if(layer != _CALC){
+      if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+          PORTB &= ~(1 << 2);
+      } else {
+          PORTB |= (1 << 2);
+      }
       if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
           PORTB &= ~(1 << 2);
       } else {
